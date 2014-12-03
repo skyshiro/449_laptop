693f76eb11d72226b91c4dcb489d44f1d9cac6a9
skyshiro <hodge.tannis@gmail.com>
2014-12-02 20:59:06 -0800

    oh ma gerd it kinda werks

--- Debug/main.pp
+++ Debug/main.pp
@@ -11,6 +11,8 @@ main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
 main.obj: ../string_ext.h
 main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdint.h
 main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stddef.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/string.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h
 
 ../main.c: 
 C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
@@ -23,3 +25,5 @@ C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h:
 ../string_ext.h: 
 C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdint.h: 
 C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stddef.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/string.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h: 
--- main.c
+++ main.c
@@ -3,27 +3,37 @@
 #include <string.h>
 #include "string_ext.h"
 
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
 //start timing from hi to low because of active low from tone decoder
 
 #define BUTTON BIT4
-#define SIGNAL BIT0
-#define TIMER_PERIOD 320		//sets timer for 20 us
-#define TIMER_CONST	50,000		//divide timercounter by this value to get frequency
+#define SIGNAL BIT6
+#define TIMER_PERIOD 20		//sets timer for 20 us
+#define TIMER_CONST	50000		//divide timercounter by this value to get frequency
+#define LCD_UPDATE 50000
+
 #define FREQ_TO_LUX	10			//to be calculated from 555 timer ckt
 
 int timerCounter; 				//Keeps track of 2us periods
 int timerCalcVal;				//Is used for calculating frequency, set to timerCounter after inputflag changes
-int inputFlag; 					//If inputFlag = 1 then waiting for low to high transition, if = 0 waiting for high to low
+int inputFlag; 					//If inputFlag = 1 then waiting for hi to lo transition, if = 0 waiting for lo to hi
+int calcFlag;					//If calcFlag = 1 calc new values for everything else do nothings
+int updateFlag;
+int frequency;
 
-void tostr(int i,char *s);
+void itoa(long unsigned int value, char* result, int base);
 
 int main(void) {
 	char frequencyMsg[] = "Freq(Hz) = ";
+	char frequencyMsgDisplay[16] = 0x00;
 	char luxMsg[] = "Lux = ";
 
 	char freqValMsg[4];
 
-	int frequency;
+
 	int lux;
 
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
@@ -36,47 +46,56 @@ int main(void) {
     P1IFG &= ~SIGNAL;
 
     //config timer for 2us period
-    CCTL0 = CCIE;				//enable interrupt
     TACTL = TASSEL_2;			//uses smclk and cont counting
-    CCR0 = TIMER_PERIOD;			//set timer to TIMERCOUNT * 16E6 seconds
+    CCTL0 |= CCIE;				//enable interrupt
+    CCR0 = TIMER_PERIOD;		//set timer to TIMERCOUNT * 16E6 seconds
 
-    inputFlag = 1;
-    timerCalcVal = 5000;
+    TA1CTL = TASSEL_2+MC_1;
+    TA1CCTL0 |= CCIE;
+    TA1CCR0 |= LCD_UPDATE;
 
+    //config LCD
     lcd_setup();
 
-    //lcd_write_message(frequencyMsg);
+    inputFlag = 1;
+    calcFlag = 0;
+    updateFlag = 1;
+    timerCalcVal = 5000;
 
-    //START TEST CODE
-    frequency = 10;
-
-    snprint_sd16(freqValMsg,4,frequency);
-
-    //tostr(frequency,freqValMsg);
-
-    lcd_write_message(strcat(frequencyMsg,freqValMsg));
-
-    //END TEST CODE
-
-    while(1);
+    frequency = TIMER_CONST / timerCalcVal;
 
     __enable_interrupt();
 
     while(1)
     {
-    	lcd_write_message(frequencyMsg+(char)frequency);
+    	//updates LCD screen with frequency value
+    	if(updateFlag)
+    	{
+    		updateFlag = 0;
 
-        if(inputFlag)
+    		frequencyMsgDisplay[16] = 0x00;
+
+			itoa(frequency,freqValMsg,10);
+			strcpy(frequencyMsgDisplay,frequencyMsg);
+			strcat(frequencyMsgDisplay,freqValMsg);
+			lcd_write_message(frequencyMsgDisplay);
+    	}
+
+        if(calcFlag)
         {
+        	calcFlag = 0;
 
-        	frequency = TIMER_CONST / timerCalcVal;
+        	if(timerCalcVal > 100)
+        	{
+        		frequency = TIMER_CONST / timerCalcVal;
+        	}
 
         	lux = frequency * FREQ_TO_LUX;
 
-        	P1IES |= SIGNAL;			//for lo/hi edge
+        	P1IE |= SIGNAL;				//enable interrupts again
+        	P1IES |= SIGNAL;			//for hi/lo edge
         }
     }
-
 
 	return 0;
 }
@@ -87,7 +106,7 @@ __interrupt void Port_1(void)
 {
 	if(P1IFG & SIGNAL)
 		{
-			P1IFG |= SIGNAL; 				// P1.4 IFG cleared
+			P1IFG &= ~SIGNAL; 				// P1.4 IFG cleared
 
 			if(inputFlag)
 			{
@@ -95,16 +114,20 @@ __interrupt void Port_1(void)
 			    timerCounter = 0;			//reset counter to start counting
 
 				P1IES &= ~SIGNAL;			//for lo/hi edge
-				TACTL |= MC_2;				//activate 2us timer
+				TACTL |= MC_1;				//activate 2us timer
+				CCTL0 |= CCIE;
 			}
 			else
 			{
 				inputFlag = 1;
-				TACTL &= ~MC_2;			//stop timer
+				TACTL &= ~MC_1;			//stop timer
+				CCTL0 &= ~CCIE;			//disable interrupt
 				P1IES |= SIGNAL;		//for hi/low edge
 				P1IE &= ~SIGNAL;		//turn off i/o interrupt while calculating frequncy
 
 				timerCalcVal = 2*timerCounter; //update the new timer value to be used in calculations
+
+				calcFlag = 1;
 			}
 
 		}
@@ -115,23 +138,37 @@ __interrupt void Port_1(void)
 __interrupt void Timer_A (void)
 {
 	timerCounter += 1;
-	CCR0 += TIMER_PERIOD;                            // Add Offset to CCR0
 }
 
-void tostr(int i,char *s) // Convert Integer to String
+// Timer A1 interrupt service routine
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void Timer_A_1 (void)
 {
-	char *p;
-	p=s;
-	p[2]=i%10;
-	i-=p[2];
-	i/=10;
-	p[1]=i%10;
-	i-=p[1];
-	i/=10;
-	p[0]=i%10;
-	i-=p[0];
-        p[3] = 0; // mark end of string
-	p[2]+=0x30;
-	p[1]+=0x30;
-	p[0]+=0x30;
+	updateFlag = 1;
+}
+
+//Here be dragons
+
+void itoa(long unsigned int value, char* result, int base)
+{
+  // check that the base if valid
+  if (base < 2 || base > 36) { *result = '\0';}
+
+  char* ptr = result, *ptr1 = result, tmp_char;
+  int tmp_value;
+
+  do {
+	tmp_value = value;
+	value /= base;
+	*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+  } while ( value );
+
+  // Apply negative sign
+  if (tmp_value < 0) *ptr++ = '-';
+  *ptr-- = '\0';
+  while(ptr1 < ptr) {
+	tmp_char = *ptr;
+	*ptr--= *ptr1;
+	*ptr1++ = tmp_char;
+  }
 }
