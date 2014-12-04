1a4920f17806e2dc472d0257a4d1ef0fcba07954
skyshiro <hodge.tannis@gmail.com>
2014-12-03 20:15:45 -0800

    its done bye

--- main.c
+++ main.c
@@ -9,11 +9,15 @@
 
 //start timing from hi to low because of active low from tone decoder
 
-#define BUTTON BIT4
+#define BUTTON BIT7
 #define SIGNAL BIT6
-#define TIMER_PERIOD 20		//sets timer for 20 us
-#define TIMER_CONST	50000		//divide timercounter by this value to get frequency
-#define LCD_UPDATE 50000
+#define TIMER_PERIOD 320		//sets timer for 20 us
+#define TIMER_CONST	50073		//divide timercounter by this value to get frequency
+#define LCD_UPDATE 16000		//sets LCD timer rate to 1 ms
+#define LCD_REFRESH 500			//update rate of LCD in ms
+#define FREQ_TO_IPD 0.792
+#define IPD_TO_FC 18.6
+#define NUM_AVG 5
 
 #define FREQ_TO_LUX	10			//to be calculated from 555 timer ckt
 
@@ -21,28 +25,40 @@ int timerCounter; 				//Keeps track of 2us periods
 int timerCalcVal;				//Is used for calculating frequency, set to timerCounter after inputflag changes
 int inputFlag; 					//If inputFlag = 1 then waiting for hi to lo transition, if = 0 waiting for lo to hi
 int calcFlag;					//If calcFlag = 1 calc new values for everything else do nothings
-int updateFlag;
+int updateCounter;
+int lcdDisplay = 0;
+int frequencyAvg[NUM_AVG];
 int frequency;
+int measureCount = -1;
 
 void itoa(long unsigned int value, char* result, int base);
 
 int main(void) {
-	char frequencyMsg[] = "Freq(Hz) = ";
-	char frequencyMsgDisplay[16] = 0x00;
-	char luxMsg[] = "Lux = ";
+	BCSCTL1 = CALBC1_16MHZ;
+	DCOCTL = CALDCO_16MHZ;
 
-	char freqValMsg[4];
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
+	const char frequencyMsg[] = "Freq(Hz) = ";
+	const char currentMsg[] = "Ipd(uA) = ";
+	const char footCandleMsg[] = "I(fc) = ";
 
-	int lux;
+	char messageDisplay[16] = 0x00;
 
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	char valueMsg[4];
+	char decimalMsg[2];
 
+	unsigned int footCandle = 0;
+	unsigned int millifootCandle = 0;
+	unsigned int milliHz = 0;
+	unsigned int photoCurrent = 0;
+	unsigned int nanoCurrent = 0;
+	int i;
     timerCounter = 0;
 
     //Config interrupts for i/o
-    P1IE |= SIGNAL;				//enable interrupt
-    P1IES |= SIGNAL;			//for hi/lo edge
+    P1IE |= SIGNAL+BUTTON;				//enable interrupt
+    P1IES |= SIGNAL;					//for hi/lo edge
     P1IFG &= ~SIGNAL;
 
     //config timer for 2us period
@@ -59,7 +75,7 @@ int main(void) {
 
     inputFlag = 1;
     calcFlag = 0;
-    updateFlag = 1;
+    updateCounter = 0;
     timerCalcVal = 5000;
 
     frequency = TIMER_CONST / timerCalcVal;
@@ -69,16 +85,61 @@ int main(void) {
     while(1)
     {
     	//updates LCD screen with frequency value
-    	if(updateFlag)
+    	if(updateCounter == LCD_REFRESH)
     	{
-    		updateFlag = 0;
+    		if(lcdDisplay > 2)
+    		{
+    			lcdDisplay = 0;
+    		}
 
-    		frequencyMsgDisplay[16] = 0x00;
+    		updateCounter = 0;
 
-			itoa(frequency,freqValMsg,10);
-			strcpy(frequencyMsgDisplay,frequencyMsg);
-			strcat(frequencyMsgDisplay,freqValMsg);
-			lcd_write_message(frequencyMsgDisplay);
+    		messageDisplay[16] = 0x00;
+
+    		BCSCTL1 = CALBC1_1MHZ;
+			DCOCTL = CALDCO_1MHZ;
+
+			if(lcdDisplay == 0)
+			{
+				//frequency
+
+				itoa(frequency,valueMsg,10);
+				itoa(milliHz,decimalMsg,10);
+				strcpy(messageDisplay,frequencyMsg);
+				strcat(messageDisplay,valueMsg);
+				strcat(messageDisplay,".");
+				strcat(messageDisplay,decimalMsg);
+				lcd_write_message(messageDisplay);
+			}
+
+			if(lcdDisplay == 1)
+			{
+				//photodiode current
+
+				itoa(photoCurrent,valueMsg,10);
+				itoa(nanoCurrent,decimalMsg,10);
+				strcpy(messageDisplay,currentMsg);
+				strcat(messageDisplay,valueMsg);
+				strcat(messageDisplay,".");
+				strcat(messageDisplay,decimalMsg);
+				lcd_write_message(messageDisplay);
+			}
+
+			if(lcdDisplay == 2)
+			{
+				//foot candle
+
+				itoa(footCandle,valueMsg,10);
+				itoa(millifootCandle,decimalMsg,10);
+				strcpy(messageDisplay,footCandleMsg);
+				strcat(messageDisplay,valueMsg);
+				strcat(messageDisplay,".");
+				strcat(messageDisplay,decimalMsg);
+				lcd_write_message(messageDisplay);
+			}
+
+			BCSCTL1 = CALBC1_16MHZ;
+			DCOCTL = CALDCO_16MHZ;
     	}
 
         if(calcFlag)
@@ -87,10 +148,41 @@ int main(void) {
 
         	if(timerCalcVal > 100)
         	{
-        		frequency = TIMER_CONST / timerCalcVal;
-        	}
+        		measureCount++;
 
-        	lux = frequency * FREQ_TO_LUX;
+        		timerCalcVal /= 10;
+
+        		if(measureCount == NUM_AVG)
+        		{
+        			measureCount = -1;
+
+        			frequency = 0;
+
+        			for(i = 0;i<NUM_AVG;i++)
+        			{
+        				frequency += frequencyAvg[i];
+        			}
+
+        			frequency /= NUM_AVG;
+
+        			//gotta do this modulo 10, divide by 10 because of decimal requirement
+
+					photoCurrent = frequency*FREQ_TO_IPD;
+
+					footCandle = photoCurrent*IPD_TO_FC;
+					millifootCandle = footCandle % 10;
+					footCandle /= 100;
+
+					nanoCurrent = photoCurrent % 100;
+					photoCurrent /= 100;
+
+					milliHz = frequency % 10;
+					frequency /= 10;								//frequency in hz
+        		}
+
+        		frequencyAvg[measureCount] = TIMER_CONST / timerCalcVal; 	//frequency in decihertz?
+
+        	}
 
         	P1IE |= SIGNAL;				//enable interrupts again
         	P1IES |= SIGNAL;			//for hi/lo edge
@@ -104,6 +196,13 @@ int main(void) {
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
+	if(P1IFG & BUTTON)
+	{
+		lcdDisplay += 1;
+
+		P1IFG &= ~BUTTON;
+	}
+
 	if(P1IFG & SIGNAL)
 		{
 			P1IFG &= ~SIGNAL; 				// P1.4 IFG cleared
@@ -144,7 +243,7 @@ __interrupt void Timer_A (void)
 #pragma vector=TIMER1_A0_VECTOR
 __interrupt void Timer_A_1 (void)
 {
-	updateFlag = 1;
+	updateCounter += 1;
 }
 
 //Here be dragons
